215c6d87330c55760bb4eafdcf31af98aa108258
dibrito <pdibrito@gmail.com>
2019-02-24 15:25:19 -0300

    order test class running

--- src/test/java/mapfood/OrderControllerTest.java
+++ src/test/java/mapfood/OrderControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,7 +24,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(OrderController.class)
+@SpringBootTest(classes = { Application.class }, webEnvironment
+        = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class OrderControllerTest {
     
     @MockBean
