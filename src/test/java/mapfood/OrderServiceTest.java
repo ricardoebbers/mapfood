329216c341c7cf7c0f992136ccd534a9216bc910
dibrito <pdibrito@gmail.com>
2019-02-24 20:13:37 -0300

    OrderServiceTest with findAndSetMotoboyTest ok DONE

--- src/test/java/mapfood/OrderServiceTest.java
+++ src/test/java/mapfood/OrderServiceTest.java
@@ -2,8 +2,10 @@ package mapfood;
 
 import mapfood.model.*;
 import mapfood.repository.ClientRepository;
+import mapfood.repository.MotoboyRepository;
 import mapfood.repository.OrderRepository;
 import mapfood.repository.RestaurantRepository;
+import mapfood.service.MotoboyService;
 import mapfood.service.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +28,9 @@ public class OrderServiceTest {
     
     @Autowired
     private OrderRepository repository;
+    
+    @Autowired
+    private MotoboyService motoBoyServive;
 
     private final String ID_CLIENT = "1";
     private final String ID_RESTAURANT = "1";
@@ -64,6 +69,37 @@ public class OrderServiceTest {
         repository.deleteById(updatedOrder.get().get_id());
         assertTrue(!repository.findById(updatedOrder.get().get_id()).isPresent());
     }
+    
+    @Test
+    public void givenOrderIdFindAndSetMotoboy() throws Exception {
+        List<OrderItem> orderItems = generateOrderItemsList();
+        
+        // create order
+        Order result = service.createOrder(ID_CLIENT, ID_RESTAURANT,orderItems);
+        assertNotNull(result);
+        assertTrue(result.getOrderStatus().equals(OrderEnum.NOVO));
+        
+        assertNull(result.getMotoboy());
+        
+        // update find and set motoboy
+        result = service.findAndSetMotoboy(result.get_id());
+        assertNotNull(result.getMotoboy());
+        assertFalse(result.getMotoboy().isAvailable());
+        
+        // turn motoboy available
+        Motoboy motoboy  = result.getMotoboy();
+        motoboy.setAvailable(true);
+        motoBoyServive.save(motoboy);
+    
+        motoboy = motoBoyServive.getById(motoboy.get_id()).get();
+        assertTrue(motoboy.isAvailable());
+    
+        // delete order
+        repository.deleteById(result.get_id());
+        assertTrue(!repository.findById(result.get_id()).isPresent());
+    }
+    
+    
     
     private Order createRadomOrder(){
     
